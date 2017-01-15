ff31cbb864dfd2875d4376473aa28c25c64e6765
Tom Taylor <tom+github@log2n.uk>
2017-01-15 22:16:56 +0000

    Cassandra as root

--- go.sh
+++ go.sh
@@ -4,4 +4,4 @@ set -e
 export LISTEN=$(hostname -I)
 envsubst <${CASSANDRA_HOME}/conf/cassandra.yaml.envsubst \
   >${CASSANDRA_HOME}/conf/cassandra.yaml
-exec bin/cassandra -f
+exec bin/cassandra -f -R
